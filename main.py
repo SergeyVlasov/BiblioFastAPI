c3b868746348251dfe6e29adef272d31901bfcda
ololoepololoe <ololoepololoe@gmail.com>
2025-01-25 16:32:59 +0300

    create tables: users, roles, books, authors

--- main.py
+++ main.py
@@ -1,75 +1,84 @@
+DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5444/test"
+
 from fastapi import FastAPI, HTTPException
 import uvicorn
 from pydantic import BaseModel, Field, ValidationError, field_validator
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5444/test.db"
-engine = create_async_engine(DATABASE_URL, echo=True)
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import sessionmaker
-async_session = sessionmaker(
-    engine, expire_on_commit=False, class_=AsyncSession
+from sqlalchemy import Column, Integer, String, Text, Date, ForeignKey
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.dialects.postgresql import ARRAY
+import asyncio
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy import Column, Integer, String, Text, Date, ForeignKey, Table
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+Base = declarative_base()
+
+book_author_association = Table(
+    "book_author_association",
+    Base.metadata,
+    Column("book_id", Integer, ForeignKey("books.id", ondelete="CASCADE"), primary_key=True),
+    Column("author_id", Integer, ForeignKey("authors.id", ondelete="CASCADE"), primary_key=True),
 )
-async def get_data():
-    async with async_session() as session:
-        async with session.begin():
-            # Your query logic here
-            pass
+
+class Author(Base):
+    __tablename__ = "authors"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255), nullable=False, unique=True)
+    biography = Column(Text, nullable=True)
+    date_of_burth = Column(Date, nullable=True)
+    books = relationship("Book", secondary=book_author_association, back_populates="authors")
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255), nullable=False)
+    description = Column(Text)
+    date_of_publication = Column(Date, nullable=False)
+    genre = Column(String(255), nullable=False)
+    count_in_stock = Column(Integer, nullable=False, default=0)
+
+    authors = relationship("Author", secondary=book_author_association, back_populates="books")
+
+user_role_association = Table(
+    "user_role_association",
+    Base.metadata,
+    Column("user_id", Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True),
+    Column("role_id", Integer, ForeignKey("roles.id", ondelete="CASCADE"), primary_key=True),
+)
+
+class Role(Base):
+    __tablename__ = "roles"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255), nullable=False, unique=True)
+    description = Column(String(255), nullable=True)
+
+    users = relationship("User", secondary=user_role_association, back_populates="roles")
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(255), nullable=False, unique=True)
+    email = Column(String(255), nullable=False, unique=True)
+    hashed_password = Column(String(255), nullable=False)
+
+    roles = relationship("Role", secondary=user_role_association, back_populates="users")
 
 
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+        print("Tables created successfully!")
 
-app = FastAPI()
-
-books = [
-    {
-        "id": 1,
-        "name": "book1",
-        "author": "author1"
-    },
-    {
-        "id": 2,
-        "name": "book2",
-        "author": "author2"
-    }
-]
-
-
-@app.get("/books")
-def read_books():
-    return books
-
-
-@app.get("/books/{book_id}")
-def read_books(book_id: int):
-    for book in books:
-        if book["id"] == book_id:
-            return book
-    raise HTTPException(status_code=404, detail="book no find")
-
-
-class Book(BaseModel):
-    title: str
-    author: str
-
-class Author(BaseModel):
-    name: str = Field(..., description="Name field that accepts only letters.")
-    bio: str = Field(..., max_length=1000)
-    born: str
-
-    @field_validator('name')
-    def only_letters(cls, value):
-        if not value.isalpha():
-            raise ValueError('The field must only contain only letters.')
-        return value
-
-@app.post("/books")
-def create_books(new_book: Book):
-   books.append({
-       "id": len(books) + 1,
-       "title": new_book.title,
-       "author": new_book.author,
-   })
-   return {"success": True}
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+asyncio.run(create_tables())
